49574eb7f234c5972dd66c408ed42c55ea9bf9e7
Glory <zamaraev@gmail.com>
2024-09-17 23:25:00 +0500

    add alembic

---
+++ alembic/env.py
@@ -0,0 +1,106 @@
+# TRUNCATE TABLE table_name RESTART IDENTITY; # чтобы сбросить сиквенсы
+# alembic revision --autogenerate -m "Init"
+# alembic upgrade head
+# alembic downgrade base
+
+import asyncio
+import sys
+from logging.config import fileConfig
+
+import alembic
+from alembic import context
+from sqlalchemy import pool
+from sqlalchemy.engine import Connection
+from sqlalchemy.ext.asyncio import async_engine_from_config
+
+
+# from src.cfg import DB_DSN
+import src.cfg as cfg
+from src.db.db import Base
+
+# from src.models import M_FILE, M_EXT, M_NSI_FIELD, M_NSI_LU, M_NSI_NGO, M_NSI_NGP, M_NSI_NGR,M_NSI_AREA, M_NSI_WELL, M_REPORT_TGF
+
+
+config = context.config
+config.set_main_option("sqlalchemy.url", cfg.DB_DSN)
+print(cfg.DB_DSN)
+# DATABASE_URL = f"postgresql://gdx2:gdx2pwd@localhost:5432/gdx2"
+# DATABASE_URL = f"postgresql+asyncpg://gdx2:gdx2pwd@localhost:5432/gdx2"
+# print(DATABASE_URL)
+DATABASE_URL = cfg.DB_DSN
+
+if config.config_file_name is not None:
+    fileConfig(config.config_file_name)
+
+# add your model's MetaData object here
+# for 'autogenerate' support
+# from myapp import mymodel
+# target_metadata = mymodel.Base.metadata
+
+
+# sys.path = ['', '..'] + sys.path[1:]
+# from src.database import Base
+
+target_metadata = Base.metadata
+
+
+
+def run_migrations_offline() -> None:
+    """Run migrations in 'offline' mode.
+
+    This configures the context with just a URL
+    and not an Engine, though an Engine is acceptable
+    here as well.  By skipping the Engine creation
+    we don't even need a DBAPI to be available.
+
+    Calls to context.execute() here emit the given string to the
+    script output.
+
+    """
+    url = config.get_main_option("sqlalchemy.url")
+    context.configure(
+        url=url,
+        target_metadata=target_metadata,
+        literal_binds=True,
+        dialect_opts={"paramstyle": "named"},
+    )
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+def do_run_migrations(connection: Connection) -> None:
+    context.configure(connection=connection, target_metadata=target_metadata)
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+async def run_async_migrations() -> None:
+    """In this scenario we need to create an Engine
+    and associate a connection with the context.
+
+    """
+
+    connectable = async_engine_from_config(
+        config.get_section(config.config_ini_section, {}),
+        prefix="sqlalchemy.",
+        poolclass=pool.NullPool,
+    )
+
+    async with connectable.connect() as connection:
+        await connection.run_sync(do_run_migrations)
+
+    await connectable.dispose()
+
+
+def run_migrations_online() -> None:
+    """Run migrations in 'online' mode."""
+
+    asyncio.run(run_async_migrations())
+
+
+if context.is_offline_mode():
+    run_migrations_offline()
+else:
+    run_migrations_online()
--- src/models.py
+++ src/models.py
@@ -2,9 +2,18 @@
 # https://github.com/alvassin/alembic-quickstart/blob/master/staff/schema.py
 # https://www.learndatasci.com/tutorials/using-databases-python-postgres-sqlalchemy-and-alembic/
 # from sqlalchemy import Table, mapped_column, Integer, String, TIMESTAMP, MetaData, TEXT, BIGINT
-from datetime import datetime
-from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import (TIMESTAMP, Boolean, Integer, String, TEXT, BigInteger, Float)
-from sqlalchemy.dialects.postgresql import TSVECTOR
 
-from src.db.db import Base
+# from datetime import datetime
+# from sqlalchemy.orm import Mapped, mapped_column
+# from sqlalchemy import (TIMESTAMP, Boolean, Integer, String, TEXT, BigInteger, Float)
+# from sqlalchemy.dialects.postgresql import TSVECTOR
+
+from sqlalchemy import MetaData
+from sqlalchemy.orm import DeclarativeBase
+
+# from src.db.db import Base
+
+from src.cfg import cfg
+
+class OrmBase(DeclarativeBase):
+    metadata = MetaData(naming_convention=cfg.CONVENTION)  # type: ignore
